2fc4f7671180bb1779f42ab0c5a013667c852fe3
bopol <bopol@e.email>
2020-06-11 15:37:18 +0200

    Change the way to accept invidious url
    now we call the api to make sure it's not offline, and no need to maintain a list of harcoded instances

--- extractor/src/test/java/org/schabi/newpipe/extractor/services/youtube/YoutubeParsingHelperTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/youtube/YoutubeParsingHelperTest.java
@@ -5,10 +5,15 @@ import org.junit.Test;
 import org.schabi.newpipe.DownloaderTestImpl;
 import org.schabi.newpipe.extractor.NewPipe;
 import org.schabi.newpipe.extractor.exceptions.ExtractionException;
+import org.schabi.newpipe.extractor.utils.Utils;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.schabi.newpipe.extractor.services.youtube.YoutubeParsingHelper.*;
 
 public class YoutubeParsingHelperTest {
     @BeforeClass
@@ -19,12 +24,20 @@ public class YoutubeParsingHelperTest {
     @Test
     public void testIsHardcodedClientVersionValid() throws IOException, ExtractionException {
         assertTrue("Hardcoded client version is not valid anymore",
-                YoutubeParsingHelper.isHardcodedClientVersionValid());
+                isHardcodedClientVersionValid());
     }
 
     @Test
     public void testAreHardcodedYoutubeMusicKeysValid() throws IOException, ExtractionException {
         assertTrue("Hardcoded YouTube Music keys are not valid anymore",
-                YoutubeParsingHelper.areHardcodedYoutubeMusicKeysValid());
+                areHardcodedYoutubeMusicKeysValid());
+    }
+
+    @Test
+    public void testIsYoutubeUrl() throws MalformedURLException {
+        final URL url = Utils.stringToURL("https://youtube.com");
+        assertTrue(isYoutubeURL(url));
+        assertFalse(isInvidiousURL(url));
+        assertFalse(isHooktubeURL(url));
     }
 }
